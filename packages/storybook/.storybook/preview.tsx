758b6627cab4bba6af196ebd681c3e3b8fa14fb4
Erik Parso <parsoerik@gmail.com>
2026-04-13 20:34:11 +0200

    dark theme docs

--- packages/storybook/.storybook/preview.tsx
+++ packages/storybook/.storybook/preview.tsx
@@ -1,11 +1,13 @@
 
-import { createMuiTheme, ThemeProvider } from '@mui/material';
+import { createMuiTheme, ScopedCssBaseline, ThemeProvider } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Preview } from "@storybook/react";
+import { themes } from 'storybook/internal/theming';
 
 const theme = createMuiTheme({
 	palette: {
+		mode: 'light',
 		primary: { main: '#1976D2' },
 		secondary: { main: '#7B3FE4' },
 	}
@@ -14,6 +16,7 @@ const theme = createMuiTheme({
 const preview: Preview = {
 	parameters: {
 		viewMode: 'docs',
+		docs: { theme: themes.dark },
 		options: {
 			storySort: {
 				order: [
@@ -30,7 +33,9 @@ const preview: Preview = {
 		(Story) => (
 			<LocalizationProvider dateAdapter={AdapterDayjs}>
 				<ThemeProvider theme={theme}>
-					<Story />
+					<ScopedCssBaseline>
+						<Story />
+					</ScopedCssBaseline>
 				</ThemeProvider>
 			</LocalizationProvider>
 		),
