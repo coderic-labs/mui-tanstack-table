23a86c6704aa2b6fb4512d76745b6d4e0c1a2877
Erik Parso <parsoerik@gmail.com>
2026-04-13 12:49:35 +0200

    basic docs

--- packages/storybook/.storybook/preview.tsx
+++ packages/storybook/.storybook/preview.tsx
@@ -4,6 +4,19 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Preview } from "@storybook/react";
 
 const preview: Preview = {
+	parameters: {
+		options: {
+			storySort: {
+				order: [
+					'Home',
+					'About',
+					'Roadmap',
+					'Changelog',
+					'Getting Started',
+				],
+			},
+		},
+	},
 	decorators: [
 		(Story) => (
 			<LocalizationProvider dateAdapter={AdapterDayjs}>
