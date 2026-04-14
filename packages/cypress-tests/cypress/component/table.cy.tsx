1f850a3e1997f1254979e9952543052e9361cd68
Erik Parso <parsoerik@gmail.com>
2026-04-14 13:39:50 +0200

    feat(cypress-tests): refactor table component tests to use Providers for context

--- packages/cypress-tests/cypress/component/table.cy.tsx
+++ packages/cypress-tests/cypress/component/table.cy.tsx
@@ -1,8 +1,13 @@
 import { ClientSideTableDemo } from '@demo-components';
+import { Providers } from '../support/providers';
 
 describe('Table', () => {
 	it('renders client side table', () => {
-		cy.mount(<ClientSideTableDemo />);
+		cy.mount(
+			<Providers>
+				<ClientSideTableDemo />
+			</Providers>
+		);
 		cy.get('table').should('be.visible');
 	});
 });
